0efe335795a45910340b326d4a485ebcb94adb6e
DayaxPro <user.name>
2023-01-01 16:34:36 -0400

    AudioMOtion

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ function AudioMotion(props: any) {
 export default function Page() {
   const [isLoading, setLoading] = useState(true);
   const [audioMotion, setAudioMotion] = useState(null);
-
+  console.log("changes");
   useEffect(() => {
     setLoading(false);
 
